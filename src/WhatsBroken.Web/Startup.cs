ee548a087ba1d50f60e0e539abd7e357b8d6d4e6
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-02-01 14:44:38 -0800

    add forwarded headers

--- src/WhatsBroken.Web/Startup.cs
+++ src/WhatsBroken.Web/Startup.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using System.Net;
 
 namespace WhatsBroken.Web
 {
@@ -41,11 +43,23 @@ namespace WhatsBroken.Web
             services.Configure<KustoOptions>(Configuration.GetSection("Kusto"));
             services.AddSingleton<AzureAuthenticationService>();
             services.AddSingleton<KustoContextFactory>();
+
+            // App Services for Containers options
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:10.0.0.0"), 104));
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:192.168.0.0"), 112));
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:172.16.0.0"), 108));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
