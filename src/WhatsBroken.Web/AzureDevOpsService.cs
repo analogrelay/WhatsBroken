63005e1f8f50e79df3a7209ff6f83964658686f1
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-02-26 08:42:39 -0800

    stash

---
+++ src/WhatsBroken.Web/AzureDevOpsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatsBroken.Web
+{
+    public class AzureDevOpsOptions
+    {
+        public string AccessToken { get; set; }
+    }
+
+    public class AzureDevOpsService
+    {
+    }
+}
